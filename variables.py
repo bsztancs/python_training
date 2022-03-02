f627a8afbad9bfcd1ab23930f88f91bae83c49a9
Résztvevő - 11 <T360-v2-DTPB-s11@oktatas.local>
2022-03-02 11:58:08 +0100

    Initial commit

---
+++ variables.py
@@ -0,0 +1,17 @@
+employee_name="John"
+color_of_eye="blue"
+print(color_of_eye)
+print(111)
+print(employee_name)
+print(type(5))
+print((type(3.14))) #float - lebegőpontos
+print((type("Hello"))) #str
+
+age=32
+print(type(age))
+age="harminckettő"
+print(type(age))
+salary:int=200
+print(salary)
+salary="kétszáz"
+print(salary)
