1bd7d10e73dc8d01693059374cdcc9216ad8327d
bsztancs <Balazs.Sztancs\telekom.com>
2022-03-02 16:54:12 +0100

    for ciklus

---
+++ month.py
@@ -0,0 +1,4 @@
+count = 1
+for month in ["január","február","március"]:
+    print("Az év " + str(count) + ".hónapja " + month + ".")
+    count+=1
---
+++ while_loop.py
@@ -0,0 +1,39 @@
+
+are_you_a_boy=True
+print(are_you_a_boy)
+
+is_17_lower_than_15=False
+print(is_17_lower_than_15)
+print(type(are_you_a_boy))
+
+
+result=10<20
+print(result)
+result=10>20
+print(result)
+
+result = 10==20
+print(result)
+
+result= 10==10
+print(result)
+print("---------------------")
+print("alma"=="alma")
+
+while False:
+    print("Szia Uram!")
+
+# while True:
+#     print("Szia Uram!")
+
+count=0
+while count <10:
+    print("Hello " + str(count))
+    count=count+1
+
+# bekert=input("Adj meg egy szamot: ")
+
+# while int(bekert)!=0:
+#     print(int(bekert)*2)
+#     bekert = input("Adj meg egy szamot: ")
+
