4c2189efca15c4e461ed3594f84e99093fd25d6f
bsztancs <Balazs.Sztancs\telekom.com>
2022-03-02 16:18:36 +0100

    for ciklus

---
+++ control_loop.py
@@ -0,0 +1,7 @@
+print("Begin")
+count=5
+for i in range(0,count):
+    print("Szia uram!")
+    print("Hello tesa")
+print("Exit")
+
---
+++ repeater.py
@@ -0,0 +1,4 @@
+name=input("mondd meg a neved: ")
+hanyszor=input("Hanyszor irjam ki? ")
+for i in range(1,int(hanyszor)+1):
+    print(str(i) + " " + name)
